cb79afedad0349dea9e27c6a8cb5ee2fee987db3
Kuldeep Kapade <kuldeepkapade@gmail.com>
2024-09-28 14:48:22 -0700

    Added django rest framework

--- gitpack/main/urls.py
+++ gitpack/main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from main.views import index
-from main.views.github_webhook_events import github_app
+from main.views.api import status
+from main.views.webhooks.github_webhook_events import github_app
 
 urlpatterns = [
     path("", index.index, name="index"),
+    path("api/status/", status.StatusView.as_view(), name="api_status"),
+
+    # Webhooks
     path('webhook/github/', github_app.github_webhook, name='github_webhook'),
 ]
