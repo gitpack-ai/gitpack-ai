3ac05ab94a1f54dfee5ff1928a6a08463e2bb128
Kuldeep Kapade <kuldeepkapade@gmail.com>
2024-10-05 14:02:47 -0700

    Updated next build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // other configurations...
+}
 
-export default nextConfig;
+export default nextConfig
